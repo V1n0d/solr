7977d17a5ee32eb5efd5b9267ffbd5a5c49348c4
V1n0d <fast2fortune@gmail.com>
2014-02-19 13:29:12 +0530

    depoly configs

--- Gemfile
+++ Gemfile
@@ -51,8 +51,9 @@ gem 'pry'
 # Use unicorn as the app server
 # gem 'unicorn'
 
-# Use Capistrano for deployment
-# gem 'capistrano', group: :development
+group :development do
+    gem 'capistrano'
+end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
---
+++ config/deploy.rb
@@ -0,0 +1,13 @@
+require 'capistrano/ext/multistage'
+
+set :application, 'Solr Sunspot Search'
+set :scm, :git
+set :repository, "git@github.com:V1n0d/solr.git"
+set :user, "root"
+set :scm_passphrase, ""
+set :branch, "master"
+set :deploy_via, :copy
+set :stages, ["staging", "development", "production"]
+set :default_stage, "development"
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
